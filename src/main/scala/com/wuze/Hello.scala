24088f80b93ee3a713ab222e60bcd171273edc7a
wuze <105222417@qq.com>
2019-04-06 21:47:45 +0800

    first commit

---
+++ src/main/scala/com/wuze/Hello.scala
@@ -0,0 +1,25 @@
+package com.wuze
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+object Hello {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("hello").setMaster("local")
+    val sc = new SparkContext(conf)
+    val input = "C:\\test\\test.txt"
+    val output = "C:\\test\\test"
+    val aa = sc.textFile(input)
+//    val bb = aa.map(x => {
+//      val pair = x.split(",")
+//      ((pair(0)),(pair(1)))
+//      println("pair(0) :" + pair(0) + "pair(1) :" + pair(1) )
+//    })
+    val wc=aa.flatMap(line =>line.split(",")).map(x=>(x,1)).reduceByKey(_+_);
+
+    //控制台输出结果
+    wc.collect().foreach(println)
+    val cc = aa.saveAsTextFile(output)
+
+  }
+
+}
