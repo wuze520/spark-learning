7efba35b96978097fbaba9bf39ed2a99b7528a12
wuze <105222417@qq.com>
2019-04-11 21:08:31 +0800

    first
    Signed-off-by: wuze <105222417@qq.com>

--- src/main/scala/com/wuze/Hello.scala
+++ src/main/scala/com/wuze/Hello.scala
@@ -6,20 +6,13 @@ object Hello {
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName("hello").setMaster("local")
     val sc = new SparkContext(conf)
-    val input = "C:\\test\\test.txt"
-    val output = "C:\\test\\test"
+    val input = args(0)
+    val output = args(1)
     val aa = sc.textFile(input)
-//    val bb = aa.map(x => {
-//      val pair = x.split(",")
-//      ((pair(0)),(pair(1)))
-//      println("pair(0) :" + pair(0) + "pair(1) :" + pair(1) )
-//    })
     val wc=aa.flatMap(line =>line.split(",")).map(x=>(x,1)).reduceByKey(_+_);
-//试试git提交功能
     //控制台输出结果
     wc.collect().foreach(println)
     val cc = aa.saveAsTextFile(output)
-
   }
 
 }
