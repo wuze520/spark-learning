75ccc15c99cccaaac65e5b14ef319d956f8fb726
105222417@qq.com <152129wq6685>
2019-10-21 17:48:30 +0800

    wuze

--- src/main/scala/com/wuze/Hello.scala
+++ src/main/scala/com/wuze/Hello.scala
@@ -6,13 +6,13 @@ object Hello {
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName("hello").setMaster("local")
     val sc = new SparkContext(conf)
-    val input = args(0)
-    val output = args(1)
+    val input = "E:\\test\\test.txt"
+    val output = "E:\\test\\result"
     val aa = sc.textFile(input)
     val wc=aa.flatMap(line =>line.split(",")).map(x=>(x,1)).reduceByKey(_+_);
     //控制台输出结果
     wc.collect().foreach(println)
-    val cc = aa.saveAsTextFile(output)
+    val cc = wc.saveAsTextFile(output)
   }
 
 }
